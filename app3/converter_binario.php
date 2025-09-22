ed6af7ed825e351f412a3fc99ac7c7d21c5fb101
Pedron444 <pedro.souza505@etec.sp.gov.br>
2025-09-22 09:20:35 -0300

    Create converter_binario.php

---
+++ app3/converter_binario.php
@@ -0,0 +1,22 @@
+<?php
+// Função para conbverter uma string para binário
+function stringParaBinario($string) {
+  // A função str_split  divide a string em um array de caracteres
+  // A função ord pega o valor ASCII de cada caractere
+  // A função decbin converte o valor ASCII em binário
+  $binario = '';
+  foreach (str_split($string) as $chat) {
+    // Converte o caractere para o valor binário de 8 bits
+    $binario .= str_pad(decbin(ord($char)), 8, '0', str_pad_left) . ' ';
+  }
+  return $binario;
+}
+// Exemplo de uso
+$frase = "Pedro Bueno de Souza";
+$fraseBinario = stringParaBinario ($frase);
+
+// Exibindo a frase original e sua versão binária
+echo "Frase original: " . $frase . "<br>";
+echo "Frase em binário: " . $fraseBinario;
+?>
+ 
